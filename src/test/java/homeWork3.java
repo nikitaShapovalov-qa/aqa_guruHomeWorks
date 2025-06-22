507ecbc0a1db1ffab5f031c8bf9634aa9a6f284d
Nikita Shapovalov <nikitka58rus2013@yandex.ru>
2025-06-22 18:39:02 +0300

    Запушил gradlew и gradlew.bat

--- src/test/java/homeWork3.java
+++ src/test/java/homeWork3.java
@@ -6,6 +6,7 @@ import static com.codeborne.selenide.Selenide.*;
 public class homeWork3 {
 
     @Test
-    void makeSomeConflicts() {
+    void demoqa_test() {
+        open("https://demoqa.com/automation-practice-form");
     }
 }
