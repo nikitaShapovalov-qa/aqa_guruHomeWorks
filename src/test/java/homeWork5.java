9d90bd0f9d5d40d8987afa21a7ee71a6cf31d6fe
Nikita Shapovalov <nikitka58rus2013@yandex.ru>
2025-10-26 15:32:08 +0300

    dz po 13

---
+++ src/test/java/homeWork5.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class homeWork5 {
+
+    ClassLoader cl = homeWork5.class.getClassLoader();
+
+    @Test
+    void zipFileTest() throws Exception {
+        try(
+                InputStream resource = cl.getResourceAsStream("priv.txt.zip");
+                ZipInputStream zip = new ZipInputStream(resource);
+                ) {
+            ZipEntry entry;
+            while((entry = zip.getNextEntry()) != null) {
+                assertThat(entry.getName()).contains("priv.txt");
+
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                zip.transferTo(baos);
+                String textContent = baos.toString(StandardCharsets.UTF_8);
+                assertThat(textContent).contains("hello my dear baby");
+            }
+        }
+    }
+}
